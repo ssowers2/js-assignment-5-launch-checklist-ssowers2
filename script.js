b42c5ba1c55fd04828904490d91317d6303012cd
ssowers2 <sabriyasowers@gmail.com>
2023-07-23 21:54:08 -0500

    Updated formSubmission() code

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
-    let form = document.querySelector("form");
-    form.addEventListener('submit', function(event) {
-        let userInput = document.querySelector("input[name=pilot name]");
-        alert("You must complete every field"); 
-    });
+    let form = document.querySelector("form");//stops the form from submitting the data and refreshing the page.
+
+    form.addEventListener('submit', function(event) { 
+    event.preventDefault(); //Stops form from submitting the data and refreshing the page.
+
+    let pilotInput = document.querySelector("input[name=pilotName]"); //represents data field
+    let pilotName = pilotInput.value; //represents field data entered
+
+    let copilotInput = document.querySelector("input[name=copilotName]");
+    let copilotName = copilotInput.value;
+
+    let fuelInput = document.querySelector("input[name=fuelLevel]");
+    let fuelLevel = Number(fuelInput.value);
+
+    let cargoInput = document.querySelector("input[name=cargoMass]");
+    let cargoMass = Number(cargoInput.value);
+
+    if (pilotName === "" || copilotName === "" || fuelLevel === "" || cargoMass === "") {
+    alert("All fields are required!"); //Alerts if all fields not completed
+
+    formSubmission(document, pilotName, copilotName, fuelLevel, cargoMass); 
+    //print same status message no matter what is in fields
+
+    }
+
+    }); 
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
@@ -17,5 +38,5 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet from the list of planets and add that information to your destination.
    })
-   
+
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -16,21 +16,55 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
    */
 }
 
-function validateInput(testInput) {
-    if (testInput === "") {
-        return "Empty";
-      }
-      else if (isNaN(testInput)) {
-        return "Not a Number";
-      }
-      else {
-        return "Is a Number";
-      }
+function validateInput(input) { //what is the point of this again if we are still validating it below?
+    if (input === "") { 
+         return "Empty";
+       } else if (isNaN(input)) {
+         return "Not a Number";
+       } else {
+         return "Is a Number";
+       }
+return input    
 
 }
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+function formSubmission(document, pilotName, copilotName, fuelLevel, cargoMass) {
+
+if(fuelLevel < 10000) { // Fuel level low 
+  document.getElementById("faultyItems").style.visibility = "visible";
+  document.getElementById("pilotStatus").innerHTML = `Pilot ${pilotName} is ready for launch`;
+  document.getElementById("copilotStatus").innerHTML = `Co-pilot ${copilotName} is ready for launch`;
+  document.getElementById("fuelStatus").innerHTML = "Fuel level too low for launch";
+  document.getElementById("launchStatus").innerHTML = "Shuttle Not Ready for Launch";
+  document.getElementById("launchStatus").style.color = "rgb(199, 37, 78)"; //red
+  
+} else if (cargoMass > 10000) { // Cargo mass large if this is if will be ready for launch
+  document.getElementById("faultyItems").style.visibility = "visible";
+  document.getElementById("pilotStatus").innerHTML = `Pilot ${pilotName} is ready for launch`;
+  document.getElementById("copilotStatus").innerHTML = `Co-pilot ${copilotName} is ready for launch`;
+  document.getElementById("cargoStatus").innerHTML = "Cargo mass too heavy for launch";
+  document.getElementById("launchStatus").innerHTML = "Shuttle Not Ready for Launch";
+  document.getElementById("launchStatus").style.color = "rgb(199, 37, 78)"; //red
+
+    
+} else if (cargoMass > 10000) { // Cargo mass large if this is if will be ready for launch
+  document.getElementById("faultyItems").style.visibility = "visible";
+  document.getElementById("pilotStatus").innerHTML = `Pilot ${pilotName} is ready for launch`;
+  document.getElementById("copilotStatus").innerHTML = `Co-pilot ${copilotName} is ready for launch`;
+  document.getElementById("cargoStatus").innerHTML = "Cargo mass too heavy for launch";
+  document.getElementById("launchStatus").innerHTML = "Shuttle Not Ready for Launch";
+  document.getElementById("launchStatus").style.color = "rgb(199, 37, 78)"; //red
+
+
+} else { //ready to lauch
+  document.getElementById("faultyItems").style.visibility = "visible";
+  document.getElementById("pilotStatus").innerHTML = `Pilot ${pilotName} is ready for launch`;
+  document.getElementById("copilotStatus").innerHTML = `Co-pilot ${copilotName} is ready for launch`;
+  document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch";
+  document.getElementById("launchStatus").style.color = "rgb(65, 159, 106)"; //green
+
+}
+
 }
 
 async function myFetch() {
@@ -48,5 +82,5 @@ function pickPlanet(planets) {
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
+module.exports.pickPlanet = pickPlanet;
 module.exports.myFetch = myFetch;
